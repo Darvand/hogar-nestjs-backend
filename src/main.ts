f7444d560b798ca2e4fbef501910a6a1f08d7853
DarvandIntelex <david.perez@intelex.com>
2023-10-03 22:30:31 -0500

    Deploy to cyclics: Add PORT 300 listening

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, () => {
+    logger.log(`Server listening in port ${process.env.PORT || 3000}`);
+  });
 }
 bootstrap();
