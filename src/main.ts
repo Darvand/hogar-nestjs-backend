48e1c55c02b4bb955ede86b4eae2b65304617a75
DarvandIntelex <david.perez@intelex.com>
2023-10-09 07:24:13 -0500

    Version 0.1.0: Add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000, () => {
     logger.log(`Server listening in port ${process.env.PORT || 3000}`);
